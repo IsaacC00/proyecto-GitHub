b824838403746cb2a2b01717489c4bc06b3f54ab
Isaac <isaacpasquel1974@gmail.com>
2023-07-03 15:01:11 -0500

    cambios en proNuevo

--- view/proNuevo.php
+++ view/proNuevo.php
@@ -5,79 +5,97 @@
 <h3>Nuevo</h3>
 
 <div class="container-fluid">
-    
-    <div class="row">
 
-        <div class="col-5">
-            <form action="post">
-                <label for="">Codigo :</label>
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">#</span>
-                <input type="text"  name="txtCodigo" class="form-control" aria-describedby="basic-addon1">
-                </div>
 
-                <label for="">Descripcion:</label>
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">#</span>
-                <input type="text"  name="txtDesc" class="form-control" aria-describedby="basic-addon1">
-                </div>
-                
-                <label for="">  <p>Precio Costo :</p> </label>
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">$</span>
-                <input type="number"  name="txtPrecio" class="form-control" aria-describedby="basic-addon1">
-                </div>
+    <form method="post">
 
-                <label for="">  <p>Precio Venta:</p> </label>
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">$</span>
-                <input type="number"  name="txtPrecioV" class="form-control" aria-describedby="basic-addon1">
-                </div>
+        <div class="row">
 
-                <label for="">  <p>Stock:</p> </label>
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">#</span>
-                <input type="number"  name="txtStock" class="form-control" aria-describedby="basic-addon1">
-                </div>
+            <div class="col-6">
+                   <div class="card card-primary">
 
-                <label for="">  <p>Fecha Elaboracion:</p> </label>
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">#</span>
-                <input type="date"  name="txtFechaElab" class="form-control" aria-describedby="basic-addon1">
-                </div>
+                    <div class="card-body">
 
-                <label for=""><p>Nivel de Azucar:</p> </label>
-                <div class="input-group mb-3">
+                        <label for="">Codigo :</label>
+                        <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">#</span>
+                        <input type="text"  name="txtCodigo" class="form-control" aria-describedby="basic-addon1">
+                        </div>
 
-                <select name="cboNivelAzucar" class="form-select" require>
-                    <option value="A">Alto</option>
-                    <option value="M">Medio</option>
-                    <option value="B">Bajo</option>
-                    <option value="N" selected>Ninguno</option>
-                </select>
+                        <label for="">Descripcion:</label>
+                        <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">#</span>
+                        <input type="text"  name="txtDesc" class="form-control" aria-describedby="basic-addon1">
+                        </div>
+                        
+                        <label for="">  <p>Precio Costo :</p> </label>
+                        <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">$</span>
+                        <input type="number"  name="txtPrecio" class="form-control" aria-describedby="basic-addon1">
+                        </div>
 
-                </div>
+                        <label for="">  <p>Precio Venta:</p> </label>
+                        <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">$</span>
+                        <input type="number"  name="txtPrecioV" class="form-control" aria-describedby="basic-addon1">
+                        </div>
 
-                <label for=""><p>Nivel de Azucar:</p> </label>
-                <div class="input-group mb-3">
-                <input type="checkbox" name="chkPagaIva" class="form-check-input">
-                <label class="form-check-label"><strong>Paga IVA</strong></label>
-                </div>
+                        <label for="">  <p>Stock:</p> </label>
+                        <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">#</span>
+                        <input type="number"  name="txtStock" class="form-control" aria-describedby="basic-addon1">
+                        </div>
 
-                <label for="">Especificaciones:</label>
-                <br>
-                <br>
-                <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">#</span>
-                <textarea name="txtEspecifi" class="form-control" id="" cols="10" rows="5"></textarea>
-                </div>
+                        <label for="">  <p>Fecha Elaboracion:</p> </label>
+                        <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">#</span>
+                        <input type="date"  name="txtFechaElab" class="form-control" aria-describedby="basic-addon1">
+                        </div>
 
-            </form>
+                        <label for=""><p>Nivel de Azucar:</p> </label>
+                        <div class="input-group mb-3">
+
+                        <select name="cboNivelAzucar" class="form-select" require>
+                            <option value="A">Alto</option>
+                            <option value="M">Medio</option>
+                            <option value="B">Bajo</option>
+                            <option value="N" selected>Ninguno</option>
+                        </select>
+
+                        </div>
+
+                        <label for=""><p>Nivel de Azucar:</p> </label>
+                        <div class="input-group mb-3">
+                        <input type="checkbox" name="chkPagaIva" class="form-check-input">
+                        <label class="form-check-label"><strong>Paga IVA</strong></label>
+                        </div>
+
+                        <label for="">Especificaciones:</label>
+                        <br>
+                        <br>
+                        <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">#</span>
+                        <textarea name="txtEspecifi" class="form-control" id="" cols="10" rows="5"></textarea>
+                        </div>
+                        </div>
+                </div> 
+
+            </div>
+
+            <div class="col-6">
+            <h3>lado derecho</h3>
+            </div>
+
+              
         </div>
 
-    </div>
+        
+    </form>
 
 </div>
+
+
+
 
 <?php
     include_once "footer.php";
